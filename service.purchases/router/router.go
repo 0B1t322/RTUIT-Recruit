47d94a38a1bf70392477af90fc11f9dbac851b53
Даня Демин <danademin@iMac-Dana.local>
2021-02-17 19:10:31 +0300

    add standart routers

---
+++ service.purchases/router/router.go
@@ -0,0 +1,22 @@
+package router
+
+import (
+	"github.com/0B1t322/RTUIT-Recruit/service.purchases/handlers"
+	"github.com/gorilla/mux"
+)
+
+func New() *mux.Router {
+	r := mux.NewRouter()
+
+	r.HandleFunc("/purchases/{id:[0-9]+}", handlers.Get).Methods("GET")
+
+	r.HandleFunc("/purchases", handlers.GetAll).Methods("GET")
+
+	r.HandleFunc("/purchases/{id:[0-9]+}", handlers.Add).Methods("POST")
+
+	r.HandleFunc("/purchases/{id:[0-9]+}", handlers.Delete).Methods("DELETE")
+
+	r.HandleFunc("/purchases/{id:[0-9]+}", handlers.Update).Methods("PUT")
+
+	return r
+}

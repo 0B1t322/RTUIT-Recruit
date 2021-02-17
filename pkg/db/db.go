60214320260df1d346ec082617f4d836951aa2a3
Даня Демин <danademin@iMac-Dana.local>
2021-02-17 20:08:04 +0300

    add package  db

---
+++ pkg/db/db.go
@@ -0,0 +1,16 @@
+package db
+
+import (
+	"github.com/0B1t322/distanceLearningWebSite/pkg/db"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func init() {
+	if _DB, err := db.DBManger.OpenDataBase("recruit"); err != nil {
+		panic(err)
+	} else {
+		DB = _DB
+	}
+}

936efeeeba7183a3ec2ddd5a6990f0fdb79edf52
Даня Демин <danademin@iMac-Dana.local>
2021-02-17 21:56:07 +0300

    add log info

--- pkg/db/db.go
+++ pkg/db/db.go
@@ -1,6 +1,7 @@
 package db
 
 import (
+	log "github.com/sirupsen/logrus"
 	"github.com/0B1t322/distanceLearningWebSite/pkg/db"
 	"gorm.io/gorm"
 )
@@ -8,6 +9,7 @@ import (
 var DB *gorm.DB
 
 func init() {
+	log.Info("recruit?parseTime=true")
 	if _DB, err := db.DBManger.OpenDataBase("recruit?parseTime=true"); err != nil {
 		panic(err)
 	} else {

9882483a77d85c79b971dd6d836a7adc20496137
Даня Демин <danademin@iMac-Dana.local>
2021-02-17 21:42:06 +0300

    dockerfile; update main and go.mod

--- service.purchases/go.mod
+++ service.purchases/go.mod
@@ -2,4 +2,8 @@ module github.com/0B1t322/RTUIT-Recruit/service.purchases
 
 go 1.15
 
-require github.com/gorilla/mux v1.8.0
+require (
+	github.com/0B1t322/RTUIT-Recruit v0.0.0-20210217170815-4c1c3e402e05
+	github.com/gorilla/mux v1.8.0
+	github.com/sirupsen/logrus v1.7.0
+)
--- service.purchases/main.go
+++ service.purchases/main.go
@@ -1,14 +1,15 @@
 package main
 
 import (
+	log "github.com/sirupsen/logrus"
 	"net/http"
 
 	"github.com/0B1t322/RTUIT-Recruit/service.purchases/router"
 )
 
-func main() {	
+func main() {
+	log.Info("Server started on :8081")
 	if err :=  http.ListenAndServe(":8081", router.New()); err  != nil {
 		panic(err)
-	}
-	
+	}	
 }

adf3153231782355b3fc62848e3686d8ecfd6edf
Даня Демин <danademin@iMac-Dana.local>
2021-02-18 20:35:40 +0300

    refactor db

--- pkg/db/db.go
+++ pkg/db/db.go
@@ -2,6 +2,9 @@ package db
 
 import (
 	log "github.com/sirupsen/logrus"
+	"github.com/0B1t322/RTUIT-Recruit/pkg/models/purchase"
+	"time"
+
 	"github.com/0B1t322/distanceLearningWebSite/pkg/db"
 	"gorm.io/gorm"
 )
@@ -11,10 +14,16 @@ const dbName = "recruit?parseTime=true"
 var DB *gorm.DB
 
 func init() {
-	log.Info(dbName)
-	if _DB, err := db.DBManger.OpenDataBase(dbName); err != nil {
+	m := db.NewManager("root", "root", "db:3306", 20*time.Second)
+
+	if _DB, err := m.OpenDataBase(dbName); err != nil {
 		panic(err)
 	} else {
 		DB = _DB
+	}
+
+	// Mirgate models
+	if err := purchase.AutoMigrate(DB); err != nil {
+		log.Error(err)		
 	}
 }

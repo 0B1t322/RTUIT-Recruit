719d1d79dd6544f4dbc386a664ac2c182da58cbb
Даня Демин <danademin@iMac-Dana.local>
2021-02-18 22:27:12 +0300

    refactor main

--- service.purchases/main.go
+++ service.purchases/main.go
@@ -1,15 +1,24 @@
 package main
 
 import (
+	"github.com/0B1t322/RTUIT-Recruit/service.purchases/app"
+	"github.com/0B1t322/RTUIT-Recruit/pkg/db"
 	log "github.com/sirupsen/logrus"
-	"net/http"
-
-	"github.com/0B1t322/RTUIT-Recruit/service.purchases/router"
 )
 
 func main() {
+	DB, err := db.DBManager.OpenDataBase(db.DBName)
+	if err != nil {
+		log.WithFields(log.Fields{
+			"Package": "main",
+			"Err": err,
+		}).Panic()
+	}
+	
+	a := app.New(DB, "8081")
+
 	log.Info("Server started on :8081")
-	if err :=  http.ListenAndServe(":8081", router.New()); err  != nil {
+	if err := a.Start(); err != nil {
 		panic(err)
 	}	
 }

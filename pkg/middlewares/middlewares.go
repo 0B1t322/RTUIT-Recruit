50c79e72528f6961b0a1bfb6b33b6ba06ccf3ba3
Даня Демин <danademin@iMac-Dana.local>
2021-02-18 17:02:33 +0300

    add setcontent-tupe middleware

--- pkg/middlewares/middlewares.go
+++ pkg/middlewares/middlewares.go
@@ -1,3 +1,13 @@
 package middlewares
 
+import "net/http"
+
 // TODO checkAuth
+
+// ContentTypeJSONMiddleware set header content-type to application json
+func ContentTypeJSONMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func (w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		next.ServeHTTP(w,r)
+	})
+}

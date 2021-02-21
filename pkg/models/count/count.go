48836d170732ef9e5cc33b12c15404fbd6e328a6
Даня Демин <danademin@iMac-Dana.local>
2021-02-21 20:56:19 +0300

    refacotr models

--- pkg/models/count/count.go
+++ pkg/models/count/count.go
@@ -4,13 +4,13 @@ import "gorm.io/gorm"
 
 type Count struct {
 	// ID of current product
-	ProductID	string 	`json:"product_id"`
+	ProductID	uint 	`json:"product_id"`
 
 	// ID of shop where this product exist
-	ShopID		string	`json:"shop_id"`
+	ShopID		uint	`json:"shop_id"`
 
 	// Count of product
-	Count		int		`json:"count"`
+	Count		uint	`json:"count"`
 }
 
 func AutoMigrate(db *gorm.DB) error {
--- pkg/models/shop/shop.go
+++ pkg/models/shop/shop.go
@@ -1,12 +1,21 @@
 package shop
 
-import "gorm.io/gorm"
+import (
+	"github.com/0B1t322/RTUIT-Recruit/pkg/models/count"
+	"github.com/0B1t322/RTUIT-Recruit/pkg/models/product"
+	"gorm.io/gorm"
+)
 
 type Shop struct {
-	ID			uint	`gorm:"primarykey"`
-	Name		string 	`json:"name"`
-	Adress		string	`json:"adress"`
-	PhoneNubmer	string	`json:"phone_number"`
+	ID			uint				`gorm:"primarykey"`
+	Name		string 				`json:"name"`
+	Adress		string				`json:"adress"`
+	PhoneNubmer	string				`json:"phone_number"`
+
+	// here all existing  products
+	Products	[]product.Product	`json:"products"`
+	// here product count
+	Counts		[]count.Count		`json:"counts"`
 }
 
 func AutoMigrate(db *gorm.DB) error {

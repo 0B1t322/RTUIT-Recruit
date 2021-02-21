509fad2451a80a465413d5bc1b3d49cccaa8ca47
Даня Демин <danademin@iMac-Dana.local>
2021-02-21 19:21:51 +0300

    add error

--- pkg/controllers/purchase/errors.go
+++ pkg/controllers/purchase/errors.go
@@ -3,5 +3,6 @@ package purchase
 import "errors"
 
 var (
-	ErrNotFound = errors.New("not found")
+	ErrNotFound 		= 	errors.New("Not found")
+	ErrInvalidShopID	=  	errors.New("Invalid ShopID: can't find shop")
 )

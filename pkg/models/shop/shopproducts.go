9ac048b802cbaed7bf2863669956367f2d9e36b8
Даня Демин <danademin@iMac-Dana.local>
2021-02-23 16:38:45 +0300

    add json tags

--- pkg/models/shop/shopproducts.go
+++ pkg/models/shop/shopproducts.go
@@ -5,9 +5,9 @@ import (
 )
 
 type ShopProducts struct {
-	ShopID		uint	`gorm:"primaryKey"`
-	ProductID	uint	`gorm:"primaryKey"`
-	Count		uint
+	ShopID		uint	`json:"shop_id" gorm:"primaryKey"`
+	ProductID	uint	`json:"product_id" gorm:"primaryKey"`
+	Count		uint	`json:"count"`
 }
 
 func (ShopProducts) BeforeCreate(db *gorm.DB) error {

d55fbca03974e11d9516d2b4220149b7353ef9c4
Даня Демин <danademin@iMac-Dana.local>
2021-02-23 18:46:49 +0300

    add field

--- pkg/models/shop/shopproducts.go
+++ pkg/models/shop/shopproducts.go
@@ -1,13 +1,16 @@
 package shop
 
 import (
+	"time"
+
 	"gorm.io/gorm"
 )
 
 type ShopProducts struct {
-	ShopID		uint	`json:"shop_id" gorm:"primaryKey"`
-	ProductID	uint	`json:"product_id" gorm:"primaryKey"`
-	Count		uint	`json:"count"`
+	ShopID		uint		`json:"shop_id" gorm:"primaryKey"`
+	ProductID	uint		`json:"product_id" gorm:"primaryKey"`
+	Count		uint		`json:"count"`
+	UpdatedAt	time.Time
 }
 
 func (ShopProducts) BeforeCreate(db *gorm.DB) error {

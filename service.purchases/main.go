5d34d55a784870320679e1ebb54042c898c9077c
Даня Демин <danademin@iMac-Dana.local>
2021-02-26 14:33:10 +0300

    add flag parse

--- service.purchases/main.go
+++ service.purchases/main.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"flag"
 	"github.com/0B1t322/RTUIT-Recruit/service.purchases/app"
 	"github.com/0B1t322/RTUIT-Recruit/pkg/db"
 	log "github.com/sirupsen/logrus"
 )
 
 func main() {
+	flag.Parse()
+
 	DB, err := db.DBManager.OpenDataBase(db.DBName)
 	if err != nil {
 		log.WithFields(log.Fields{

1aa7bcdbbb48e3892f1c6223900f4c2bf11c103f
Даня Демин <danademin@iMac-Dana.local>
2021-02-27 20:00:22 +0300

    refactor model

--- pkg/models/shop/shopproducts.go
+++ pkg/models/shop/shopproducts.go
@@ -1,36 +1,40 @@
 package shop
 
 import (
+	pc "github.com/0B1t322/RTUIT-Recruit/pkg/controllers/product"
 	"time"
 
 	"github.com/0B1t322/RTUIT-Recruit/pkg/models/product"
 	"gorm.io/gorm"
 )
 
-type ShopProducts struct {
-	ShopID		uint		`json:"shop_id" gorm:"primaryKey"`
-	ProductID	uint		`json:"product_id" gorm:"primaryKey"`
-
-	// TODO make foreign key here for product
-
-	Count		uint		`json:"count"`
-	UpdatedAt	time.Time
-}
-
-func (ShopProducts) BeforeCreate(db *gorm.DB) error {
-	return nil
-}
-
-func SetupJoinTable(db *gorm.DB) error {
-	return db.SetupJoinTable(&Shop{}, "Products", &ShopProducts{})
-}
-
 type ShopProduct struct {
-	ShopID		uint			`json:"shop_id"`
-	ProductID	uint			`json:"product_id"`
+	ShopID		uint			`json:"shop_id" gorm:"primaryKey"`
+	ProductID	uint			`json:"product_id" gorm:"primaryKey"`
 	Product		product.Product	`json:"product" gorm:"foreignKey:ID;references:ProductID"`
 
 	Count		uint			`json:"count"`
 
 	UpdatedAt	time.Time
+}
+
+func (sp *ShopProduct) AfterCreate(tx *gorm.DB) error {
+	return sp.findProduct(tx)
+}
+
+func (sp *ShopProduct) AfterFind(tx *gorm.DB) error {
+	return sp.findProduct(tx)
+}
+
+func (sp *ShopProduct) findProduct(tx *gorm.DB) error {
+	if sp.ProductID != 0 {
+		err := tx.First(&sp.Product, "id = ?", sp.ProductID).Error
+		if err == gorm.ErrRecordNotFound {
+			return pc.ErrNotFound
+		} else if  err !=  nil {
+			return err
+		}
+	}
+
+	return nil
 }

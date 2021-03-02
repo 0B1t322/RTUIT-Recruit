639a83b2827cc314f42d5494ddb241268292296c
0B1t322 <dan.demin@gmail.com>
2021-03-02 23:32:29 +0300

    refactor

--- pkg/controllers/purchase/errors.go
+++ pkg/controllers/purchase/errors.go
@@ -3,7 +3,7 @@ package purchase
 import "errors"
 
 var (
-	ErrNotFound 		= 	errors.New("Not found")
+	ErrNotFound 		= 	errors.New("Purchase not found")
 	ErrInvalidShopID	=  	errors.New("Invalid ShopID: can't find shop")
 	ErrInvalidProductID	=	errors.New("Invalid ProductID: can't find product")
 )

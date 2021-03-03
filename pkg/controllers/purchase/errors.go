1cf066061cbd08d6016adef0ace76207b181c906
0B1t322 <dan.demin@gmail.com>
2021-03-03 15:59:08 +0300

    add error

--- pkg/controllers/purchase/errors.go
+++ pkg/controllers/purchase/errors.go
@@ -6,4 +6,5 @@ var (
 	ErrNotFound 		= 	errors.New("Purchase not found")
 	ErrInvalidShopID	=  	errors.New("Invalid ShopID: can't find shop")
 	ErrInvalidProductID	=	errors.New("Invalid ProductID: can't find product")
+	ErrCountNull 		= 	errors.New("Count can't be null")
 )

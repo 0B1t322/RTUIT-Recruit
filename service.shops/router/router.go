ae4015497cd28d95a32a8e923a43b96eb4fbfe04
0B1t322 <dan.demin@gmail.com>
2021-03-03 21:51:40 +0300

    update route

--- service.shops/router/router.go
+++ service.shops/router/router.go
@@ -1,28 +1,46 @@
 package router
 
 import (
-	_ "github.com/0B1t322/RTUIT-Recruit/pkg/middlewares"
+	"net/http"
+
+	"github.com/0B1t322/RTUIT-Recruit/pkg/middlewares"
+	"github.com/0B1t322/RTUIT-Recruit/service.shops/handlers"
 	"github.com/gorilla/mux"
+	"gorm.io/gorm"
 )
 
-func New() *mux.Router {
+func New(db *gorm.DB) *mux.Router {
 	r := mux.NewRouter()
-
+	h := handlers.New(db)
+	h.PurhacesNetwork = "http://localhost:8081"
 	// Get Shop
-	r.HandleFunc("/shops/{id:[0-9]+}", nil).Methods("GET")
+	r.HandleFunc("/shops/{id:[0-9]+}", h.Get).Methods("GET")
 
 	// Buy product
-	r.HandleFunc("/shops/{id:[0-9]+}/{pid:[0-9]+}", nil).Methods("PUT")
+	r.HandleFunc("/shops/{id:[0-9]+}/{pid:[0-9]+}", h.Buy).Methods("PUT")
 
 	// Check purchases
-	r.HandleFunc("/shops/{uid:[0-9]+}", nil).Methods("GET")
+	r.HandleFunc("/shops/purchases/{uid:[0-9]+}", h.GetPurchases).Methods("GET")
 
-	// Add product
+	// Add count product
 	// this method only for fabrics
-	r.Handle("/shops/{id:[0-9]+}/{pid:[0-9]+}", nil).Methods("POST")
+	r.Handle(
+			"/shops/{id:[0-9]+}/{pid:[0-9]+}/{count:[0-9]+}", 
+			middlewares.CheckTokenIfFromService(
+				http.HandlerFunc(h.AddCount),
+			),
+		).Methods("PUT")
+
+	// Add product to shop
+	r.Handle(
+			"/shops/{id:[0-9]+}/{pid:[0-9]+}", 
+			middlewares.CheckTokenIfFromService(
+				http.HandlerFunc(h.AddProduct),
+			),
+		).Methods("POST")
 
 	// Get All Shop
-	r.HandleFunc("/shops", nil).Methods("GET")
+	r.HandleFunc("/shops", h.GetAll).Methods("GET")
 
 	return r
 }

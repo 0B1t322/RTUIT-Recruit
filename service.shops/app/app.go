5f29cc6084fb7a1a29a24a5bacfba0d795d03ea3
0B1t322 <dan.demin@gmail.com>
2021-03-06 14:33:55 +0300

    add main and app

--- service.shops/app/app.go
+++ service.shops/app/app.go
@@ -1,1 +1,54 @@
 package app
+
+import (
+	"fmt"
+	"net/http"
+	"github.com/0B1t322/RTUIT-Recruit/pkg/models/product"
+	"github.com/0B1t322/RTUIT-Recruit/pkg/models/shop"
+	"github.com/sirupsen/logrus"
+	"github.com/0B1t322/RTUIT-Recruit/pkg/middlewares"
+	"github.com/0B1t322/RTUIT-Recruit/service.shops/handlers"
+	"github.com/0B1t322/RTUIT-Recruit/service.shops/router"
+	"github.com/0B1t322/RTUIT-Recruit/pkg/app"
+	"github.com/gorilla/mux"
+	"gorm.io/gorm"
+)
+
+type App struct {
+	db *gorm.DB
+
+	port string
+
+	r *mux.Router
+}
+
+func New(DB *gorm.DB, Port, PurhacesNetwork string) app.App {
+	return &App{
+		db: DB,
+		port: Port,
+		r: router.New(
+			handlers.New(DB, PurhacesNetwork),
+		),
+	}
+}
+
+func (a *App) Start() error {
+	if err := a.init(); err != nil {
+		return err
+	}
+
+	return http.ListenAndServe(fmt.Sprintf(":%s", a.port), a.r)
+}
+
+func (a *App) init() error {
+	middlewares.Logger = logrus.StandardLogger()
+	if err := shop.AutoMigrate(a.db); err != nil {
+		return err
+	}
+
+	if err := product.AutoMigrate(a.db); err != nil {
+		return err
+	}
+
+	return nil
+}
--- service.shops/main.go
+++ service.shops/main.go
@@ -1,2 +1,29 @@
 package main
 
+import (
+	"github.com/0B1t322/RTUIT-Recruit/pkg/db"
+	log "github.com/sirupsen/logrus"
+	
+	"github.com/0B1t322/RTUIT-Recruit/service.shops/app"
+	"flag"
+)
+
+func main() {
+	flag.Parse()
+
+	DB, err := db.DBManager.OpenDataBase(db.DBName)
+	if err != nil {
+		log.WithFields(log.Fields{
+			"Package": "main",
+			"Err": err,
+		}).Panic()
+	}
+
+	a := app.New(DB, "8082", "service.purchases:8081")
+
+	log.Infoln("Starting server on :8082")
+
+	if err := a.Start(); err != nil {
+		panic(err)
+	}
+}

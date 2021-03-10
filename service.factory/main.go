cb94dc788f3e49ef3c90b8516bd1d19d3d565c04
Даня Демин <danademin@iMac-Dana.local>
2021-03-11 00:04:08 +0300

    update main.go

--- service.factory/main.go
+++ service.factory/main.go
@@ -1,30 +1,26 @@
 package main
 
 import (
-	"github.com/0B1t322/distanceLearningWebSite/pkg/db"
+	"github.com/0B1t322/RTUIT-Recruit/pkg/db"
 	"flag"
-	"github.com/sirupsen/logrus"
 	"os"
 	"os/signal"
 	"syscall"
-	"time"
+
+	"github.com/sirupsen/logrus"
 
 	pc "github.com/0B1t322/RTUIT-Recruit/pkg/controllers/product"
-	"github.com/0B1t322/RTUIT-Recruit/pkg/models/product"
 
 	"github.com/0B1t322/RTUIT-Recruit/service.factory/app"
-	"github.com/0B1t322/RTUIT-Recruit/service.factory/factory"
 )
-
-const ShopNetwork = "http://localhost:8082"
 
 var creater *pc.ProductController
 
 var done chan os.Signal
 
 func init() {
-	m := db.NewManager("root", "root", "127.0.0.1:3306", 15*time.Second)
-	db, err := m.OpenDataBase("recruit?parseTime=true")
+	// m := db.NewManager("root", "root", "127.0.0.1:3306", 15*time.Second)
+	db, err := db.DBManager.OpenDataBase("recruit?parseTime=true")
 	if err != nil {
 		panic(err)
 	}
@@ -35,24 +31,15 @@ func init() {
 
 func main() {
 	flag.Parse()
-	logrus.SetLevel(logrus.ErrorLevel)
+	// logrus.SetLevel(logrus.ErrorLevel)
+	cfg, err := app.ParseCfg("config.json")
+	if err != nil {
+		logrus.Panicf("Failed to parse config: %v", err)
+	}
+
 	app := app.New(
-		factory.NewFactory(
-			15 * time.Second,
-			3,
-			[]product.Product{
-				{Name: "iPhone X 64 GB", Desccription: "New iphone with 64 gb", Cost: 57000.0, Category: "Phones"},
-				{Name: "iPhone X 128 GB", Desccription: "New iphone with 128 gb", Cost: 77000.0, Category: "Phones"},
-				{Name: "iPhone X 256 GB", Desccription: "New iphone with 256 gb", Cost: 97000.0, Category: "Phones"},
-			},
-			creater,
-		),
-		factory.NewDelivary(
-			ShopNetwork,
-			10 * time.Second,
-			5 * time.Second,
-		),
-		factory.NewWarehouse(),
+		cfg,
+		creater,
 	)
 
 	logrus.Infoln("Start service factory")
